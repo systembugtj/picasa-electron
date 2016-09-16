f671167152141da98226a2a508616a9166015332
Albert <albert.li@salesforce.com>
2016-09-16 16:25:52 -0700

    Add Path View

--- app/pods/components/path-tree/component.js
+++ app/pods/components/path-tree/component.js
@@ -1,4 +1,35 @@
 import Ember from 'ember';
+import TreeNode from 'ember-idx-tree/node'
 
 export default Ember.Component.extend({
+  init() {
+    var family, gaya, josh, lud, moses, suz, verdi;
+    family = TreeNode.create({
+      title: 'Family'
+    });
+    suz = family.createChild({
+      title: 'Susan'
+    });
+    lud = family.createChild({
+      title: 'Luda'
+    });
+    josh = suz.createChild({
+      title: 'Josh'
+    });
+    moses = suz.createChild({
+      title: 'Moses'
+    });
+    verdi = lud.createChild({
+      title: 'Verdi'
+    });
+    gaya = lud.createChild({
+      title: 'Gaya'
+    });
+    return this.set('model', family);
+  },
+  actions: {
+    expand() {
+      this.get('selected').toggleProperty('expanded');
+    }
+  }
 });
