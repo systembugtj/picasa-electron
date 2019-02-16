dc0c64d1210638b66b65e999f740ff6c32c725e1
systembugtj <albert_lee@hotmail.com>
2019-02-15 18:06:52 -0800

    style

--- src/app/pods/components/folder-header/component.js
+++ src/app/pods/components/folder-header/component.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
-
-export default class FolderHeaderComponent  extends Component {
+import { classNames } from '@ember-decorators/component';
+import I18nMixin from 'ember-i18next/mixins/i18n';
+@classNames("folder-header")
+class FolderHeaderComponent  extends Component.extend(I18nMixin) {
 }
+
+export default FolderHeaderComponent;
